ec61cffe310b3b02f2314e66d69222bb612b69bc
Er3te <72126994+Er3te@users.noreply.github.com>
2026-09-17 22:41:52 -0400

    Added image

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ function App(): React.JSX.Element {
             <h1 className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </h1>
+            <img
+                src="https://www.freecodecamp.org/news/content/images/2024/04/GitHub-1.png"
+                alt="A picture GitHub's landing page."
+            />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Ray Hello World
